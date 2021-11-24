f90adfc029b23ad38e3c70e3cd259b36f1a962c2
ClaudeFalzon <cjfalzon@gmail.com>
2021-11-24 14:10:20 +0100

    last

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
-
+/* import World from "../src/images/img.jpg"
+import Covid from "../src/images/img1.jpg" */
 function App() {
   const [value, setValue] = useState();
 
@@ -10,6 +11,9 @@ function App() {
   return (
     <div className="App">
       <h1>My React App</h1>
+
+{/*       <img src={World} alt="img" />
+      <img src={Covid} alt="img" /> */}
 
       <PhoneInput
         placeholder="Enter phone number"
